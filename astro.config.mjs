d30b35be9f42ba797f247acb0f5199357233a873
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2024-12-22 17:26:57 +0100

    another

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apps.xrun.gdn/',
-
+  server: {
+    host: '0.0.0.0'
+  },
 });
